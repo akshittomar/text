2ecb5a7850ac45971e8e34cca5577c7cfbb0873f
Akshit Tomar <akshitt125@gmail.com>
2023-08-09 22:23:22 +0530

    OPTIMISED

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
    <Navbar  mode={mode} setmode={setmode} />
    </div>
    <Routes>
-    <Route exact path='/' element={ <div className='my-4 mx-3'><TextArea hi="hello" heading="ENTER TEXT HERE " alert={Dialog}  message={setMessage} mode={mode} setmode={setmode} /></div>}></Route>
+    <Route exact path='/' element={ <div className='my-4 mx-3'><TextArea hi="hello" heading="ENTER TEXT HERE" alert={Dialog}  message={setMessage} mode={mode} setmode={setmode} /></div>}></Route>
    <Route exact path='/about' element={<div className='my-5 mx-4'><About  mode={mode} setmode={setmode}/></div>} ></Route>
    <Route exact path='/features' element={ <Feature  mode={mode} setmode={setmode}/>}></Route>
    <Route exact path='/price' element={ <Price  mode={mode} setmode={setmode}/>}></Route>
